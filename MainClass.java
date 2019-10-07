a3133884c5019e5ba2fb3fdd0ba79a9ee364a6ab
hujinlon <55047319+hujinlon@users.noreply.github.com>
2019-10-07 11:19:21 +0800

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+import course.*;
+import teacher.*;
+import student.*;
+public class MainClass
+{
+	public static void main(String[] args) {
+		Course cs=new Course();
+		Student st=new Student();
+		Teacher te=new Teacher();
+	}
+}
